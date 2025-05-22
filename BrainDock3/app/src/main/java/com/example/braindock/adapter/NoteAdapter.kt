ff55df76a4d840e5420ba09f956de3fe38396a58
Anastasiia Kharytonova <nharytonova@gmail.com>
2025-05-22 14:38:37 +0300

    add adapter, view model(factory)

---
+++ BrainDock3/app/src/main/java/com/example/braindock/adapter/NoteAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.braindock.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.braindock.fragments.HomeFragmentDirections
+import com.example.notesroompractice.databinding.NoteLayoutBinding
+import com.example.braindock.model.Note
+
+
+
+
+class NoteAdapter: RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    class NoteViewHolder(val itemBiding: NoteLayoutBinding): RecyclerView.ViewHolder(itemBiding.root)
+
+    private  val differCallback = object : DiffUtil.ItemCallback<Note>(){
+        override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem.id == newItem.id &&
+                    oldItem.noteDesc == newItem.noteDesc &&
+                    oldItem.noteTitle == newItem.noteTitle
+        }
+
+        override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+                return oldItem == newItem
+            }
+    }
+    val differ  = AsyncListDiffer(this,differCallback)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        return NoteViewHolder(
+            NoteLayoutBinding.inflate(LayoutInflater.from(parent.context),parent)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val currentNote = differ.currentList[position]
+
+        holder.itemBiding.noteTitle.text=currentNote.noteTitle
+        holder.itemBiding.noteDesc.text=currentNote.noteDesc
+
+        holder.itemView.setOnClickListener {
+            val direction = HomeFragmentDirections.actionHomeFragmentToEditNoteFragment(currentNote)
+            it.findNavController().navigate(direction)
+        }
+
+    }
+
+}
